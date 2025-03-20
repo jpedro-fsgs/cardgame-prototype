7eecc7c834e7c66b25911d2ffc424574e8a07498
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2025-03-20 14:14:58 -0300

    initial commit

---
+++ scripts/card_slot.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var cards_in_slot = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func add_to_slot(card):
+	cards_in_slot.append(card)
+	
+	print(cards_in_slot)
